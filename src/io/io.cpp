c9bd7127ef783007a2eb57bfe7a199e3dcd9fade
Gleb Salmanov <nullcaller@gmail.com>
2026-01-31 23:48:19 +0300

    chore: mark down structure for source tree abstraction
    Signed-off-by: Gleb Salmanov <nullcaller@gmail.com>

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -1,4 +1,23 @@
+#include<fstream>
+
 #include<util/namespace.hpp>
 
 #include "io.hpp"
 using namespace relog;
+
+class SourceTree
+{
+
+};
+	
+class SourceTreeNode
+{
+	protected: 
+	enum Type
+	{
+		DIRECTORY,
+		FILE
+	};
+
+
+};
