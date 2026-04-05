240ca4ff2af0dc81ee7be8828291e930b7a6fa9c
Gleb Salmanov <nullcaller@gmail.com>
2026-04-05 15:54:52 +0300

    feat(bootstrap): log using logging service
    Signed-off-by: Gleb Salmanov <nullcaller@gmail.com>

--- src/bootstrap/bootstrap.cpp
+++ src/bootstrap/bootstrap.cpp
@@ -2,9 +2,42 @@
 
 #include <bootstrap/opts.cpp>
 
+#include <service/logging/log_domain.cpp>
+#include <service/logging/logger.cpp>
+#include <service/logging/logger_backend_terminal.cpp>
+#include <service/logging/logger_formatter_terminal.cpp>
+#include <service/logging/loggerf.cpp>
+
+#include <lib/domain/domain.cpp>
+
+#include <memory>
+
+namespace relog::bootstrap
+{
+
+relog::service::logging::LogDomainOwner<std::string> d{};
+relog::service::logging::LogDomainProxy<std::string> r = d.get("bootstrap");
+relog::service::logging::Loggerf l{r, relog::service::logging::LogLevel::INFO};
+
+void
+init_logging()
+{
+	auto backend = std::make_shared<relog::service::logging::LoggerBackendTerminal>();
+	auto formatter = std::make_shared<relog::service::logging::LoggerFormatterTerminalPlain>();
+
+	r.changePipeline(backend, formatter);
+	r.setThreshold(relog::service::logging::LogLevel::INFO);
+}
+
+} // namespace relog::bootstrap
+
 int
 main(int argc, char* argv[])
 {
+	using relog::bootstrap::l;
+
+	relog::bootstrap::init_logging();
 	relog::bootstrap::parse_opts(argc, argv);
+
 	return 0;
 }
--- src/bootstrap/bootstrap.hpp
+++ src/bootstrap/bootstrap.hpp
@@ -1,4 +1,21 @@
 #ifndef RELOG_BOOTSTRAP_BOOTSTRAP_HPP
 #define RELOG_BOOTSTRAP_BOOTSTRAP_HPP
 
+#include <service/logging/log_domain.hpp>
+#include <service/logging/loggerf.hpp>
+
+#include <string>
+
+namespace relog::bootstrap
+{
+
+extern relog::service::logging::LogDomainOwner<std::string> d;
+extern relog::service::logging::LogDomainProxy<std::string> r;
+extern relog::service::logging::Loggerf l;
+
+void
+init_logging();
+
+} // namespace relog::bootstrap
+
 #endif
